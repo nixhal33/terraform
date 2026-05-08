5e64ad8bc7683b11992276181f58a4379efad0e6
nixhal33 <nischal.koirala33@gmail.com>
2026-05-08 23:42:27 +0545

    subnet added

--- s3-vpc-connect/main.tf
+++ s3-vpc-connect/main.tf
@@ -11,10 +11,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "vpc_01_tf" {
+resource "aws_vpc" "vpc-1" {
     cidr_block = "10.0.0.0/16"
     instance_tenancy = "default"
     tags = {
         Name = "vpc-s3-connect"
+    }
+}
+
+resource "aws_subnet" "subnet_01_tf" {
+    vpc_id = aws_vpc.vpc-1.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+    map_public_ip_on_launch = true
+    tags = {
+        Name = "subnet-1"
     }
 }
